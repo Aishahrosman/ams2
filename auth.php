4f5acadb67241f8ad87a108451213ecb794b7876
MYTV-Siti Nooraishah <nooraishah@mtvb.my>
2022-04-25 15:14:18 +0800

    new

--- auth.php
+++ auth.php
@@ -1,56 +1,33 @@
-<?php include_once('includes/load.php'); 
-include('includes/database.php');
-
-$req_fields = array('username','password');
+<?php include_once('includes/load.php'); ?>
+<?php
+$req_fields = array('username','password' );
 validate_fields($req_fields);
 $username = remove_junk($_POST['username']);
 $password = remove_junk($_POST['password']);
 
   if(empty($errors)){
 
-    // $user = authenticate_v2($username, $password);
+    $user = authenticate_v2($username, $password);
 
-    global $db;
-     $useremail = $db->escape($username);
-     $password = $db->escape($password);
-     $sql  = sprintf("SELECT id,username,password,email,role FROM `user` WHERE username='$useremail' AND password='$password'");
-     $result = $db->query($sql);
-     if($db->num_rows($result)){
-       $user = $db->fetch_assoc($result);
-       $password_request = sha1($password);
-       if($password_request === $user['password'] ){
-         return $user;
-       }
-     }
-    return false;
-
-        if($user) 
-        {
+        if($user):
            //create session with id
            $session->login($user['id']);
            //Update Sign in time
            updateLastLogIn($user['id']);
            // redirect user to group home page by user level
-           if($user['role'] === 'admin')
-           {
-            //  $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+           if($user['role'] === 'admin'):
+             $session->msg("s", "Hello ".$user['username'].", Welcome.");
              redirect('dashboard.php',false);
-           }
-           else if ($user['role'] === 'user')
-           {
-              // $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
+           elseif ($user['role'] === 'user'):
+              $session->msg("s", "Hello ".$user['username'].", Welcome .");
              redirect('dashboard2.php',false);
-          //  else:
-          //     $session->msg("s", "Hello ".$user['username'].", Welcome to OSWA-INV.");
-          //    redirect('home.php',false);
-          }
-        }
+           
+           endif;
 
-        else
-        {
-          $session->msg("d", "Sorry Username/Password incorrect. '$username'");
-          redirect('dashboard.php',false);
-        }
+        else:
+          $session->msg("d", "Sorry Username/Password incorrect.");
+          redirect('index.php',false);
+        endif;
 
   } else {
 
--- index.php
+++ index.php
@@ -1,47 +1,5 @@
-<?php
-  ob_start();
-  require_once('includes/load.php');
-  if($session->isUserLoggedIn(true)) { redirect('dashboard.php', false);}
-?>
-
-<?php include_once('layouts/header-login.php'); ?>
+<?php include ('layouts/sidebar.php')?>
 
 
-<div class="page-form">
 
-    <form action="auth.php" class="form" method="post">
-        <div class="header-content"><h1>Log In AMS</h1></div>
-        <div class="body-content"><p></p>
-        <?php echo display_msg($msg); ?>
-            <div class="form-group">
-                <div class="input-icon right"><i class="fa fa-user"></i>
-                  <input type="email" placeholder="Username" name="username" id="username" class="form-control">
-                </div>
-            </div>
-            
-            <div class="form-group">
-                <div type = 'css/text' class="input-icon right"><i class="fa fa-key"></i>
-                 <input type="password" placeholder="Password" name="password" id="password" class="form-control" 
-                  >
-                </div>
-            </div>
-            <div class="form-group pull-left">
-                
-            </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-info  pull-right">Login</button>
-        </div>
-            <div class="clearfix"></div>
-            <div class="forget-password"><h4>Forgotten your Password?</h4>
-
-                <p>no worries, click <a href='forgot.php'><font color="blue">here</font></a> to reset your password.</p></div>
-            <hr>
-              
-    </form>
-	
-</div>
-		<!--BEGIN FOOTER-->
-        <?php include_once('layouts/footer.php'); ?>
-        <!--END FOOTER--><!--END PAGE WRAPPER-->
-</div>
-
+<?php include ('layouts/footer.php')?>
